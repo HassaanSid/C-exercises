ce292a63658249b2e8c3a7be67a0dadd5952bf76
HassaanSid <hassaanahmadsiddiqui@gmail.com>
2024-10-11 10:03:40 +0500

    Add solution of Q6

--- book-solutions/chapt-10/Q6.c
+++ book-solutions/chapt-10/Q6.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <ctype.h>
 
 #define STACK_SIZE 100
 
@@ -23,18 +24,72 @@ bool is_full()
 
 void push (int i)
 {
-	if (is_full) printf("Stack overflow\n");
+	if (is_full()) printf("Stack overflow\n");
 	contents[top++]=i;
 }
 
 int pop (void)
 {
-	if(is_empty) printf("Stack underflow\n");
+	if(is_empty()) printf("Stack underflow\n");
 	return contents[--top];
 }
 
+void add()      {top>=2 ? push(pop() + pop()) : printf("\nNot enough operands!");}
+void multiply() {top>=2 ? push(pop() * pop()) : printf("\nNot enough operands!");}
+
+void subtract() {
+    if (top >= 2) {
+        int s = pop();
+        push(pop() - s);
+    } else {
+        printf("\nNot enough operands!\n");
+    }
+}
+
+
+void divide() {
+    if (top >= 2) {
+        int d = pop();
+        push(pop() / d);
+    } else {
+        printf("\nNot enough operands!\n");
+    }
+}
+
+
 int main()
 {
+	char input[100], c;
+	printf("Kindly enter the RPN expression: ");
+	scanf("%s", input); //the %s doesnot limit the input to a specific number of characters, therefore we must limit using %9s or something
+	printf("\n %s", input);
 
+	make_empty();
+
+	for(int i=0; input[i] != '\0'; i++)
+	{
+		c=input[i];
+		if (isdigit(c))
+		{
+			push(c - '0');
+		}
+		else if (c == '=')
+		{
+			printf("\nThe result is: %d", pop());
+		}
+		else
+		{
+			switch(c)
+			{
+				case '+': add(); break;
+				case '-': subtract(); break;
+				case '/': divide(); break;
+				case '*': multiply(); break;
+				default : printf("\n What the fuck man??");
+			}
+		}
+	}
+	
 	return 0;
 }
+
