80f50bf486e47eb529604322ecd175e3a9364cea
HassaanSid <hassaanahmadsiddiqui@gmail.com>
2024-10-23 08:56:14 +0500

    Add Q1b.c

---
+++ book-solutions/chapt-12/Q1b.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+
+void user_input(char [], int);
+void reverse_string(char []);
+
+int main() {
+    
+    char message[100];
+    user_input(message, sizeof(message));
+    printf("You entered: %s\n", message);
+    reverse_string(message);
+    printf("Reverse: %s\n", message);
+
+    return 0;
+}
+
+void user_input(char input[], int size) {
+    printf("\nEnter the message: ");
+    if (fgets(input, size, stdin) != NULL) {
+        // Remove the newline character if present
+        size_t len = strlen(input);
+        if (len > 0 && input[len - 1] == '\n') {
+            input[len - 1] = '\0';
+        }
+    }
+}
+
+void reverse_string(char input[])
+{
+    char *start = input;
+    char *end = input +strlen(input)-1;
+    char temp;
+
+    while(start < end)
+    {
+        temp = *start;
+        *start++ = *end;
+        *end-- = temp;    
+    }
+}
